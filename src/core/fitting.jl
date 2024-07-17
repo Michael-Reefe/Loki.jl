b8834936cd4698c8bfaded7afc92b20c23d6657c
Michael-Reefe <michael.reefe8@gmail.com>
2024-07-16 23:06:42 -0400

    updated evaluate_model function

--- src/core/fitting.jl
+++ src/core/fitting.jl
@@ -1820,11 +1820,9 @@ function evaluate_model(λ::Vector{<:Real}, spaxel::CartesianIndex, cube_fitter:
         for (i, ch_mask) ∈ enumerate(cube_fitter.channel_masks)
             fit_norm = 10 .^ read(hdu_param["TEMPLATES.NUCLEAR.AMP_$i"])         # Fit amplitudes
             fit_norm[.~isfinite.(fit_norm)] .= 1.
-            for k ∈ (1:size(psf_norm, 3))[ch_mask]
-                for ki ∈ k
-                    s = nansum(psf_norm[:, :, ki] .* fit_norm) 
-                    psf_norm[:, :, ki] ./= s                                     # Dividing by the sum of (PSF) x (fit amp)
-                end
+            for k ∈ findall(ch_mask)
+                s = nansum(psf_norm[:, :, k] .* fit_norm) 
+                psf_norm[:, :, k] ./= s                                     # Dividing by the sum of (PSF) x (fit amp)
             end                                                                  
         end
 
@@ -1852,32 +1850,132 @@ function evaluate_model(λ::Vector{<:Real}, spaxel::CartesianIndex, cube_fitter:
 
         # Create new channel masks carefully -- dont add new channel masks if the extrapolation extends into other 
         # wavebands that could've been covered, just add them into the existing channel masks
-        λ_overlap = λ[minimum(cube_fitter.cube.λ) .≤ λ .≤ maximum(cube_fitter.cube.λ)]
-        λ_left = λ[λ .< minimum(cube_fitter.cube.λ)]
-        L_left = length(λ_left)
-        pad_left = falses(L_left)
-        λ_right = λ[λ .> maximum(cube_fitter.cube.λ)]
-        L_right = length(λ_right)
-        pad_right = falses(L_right)
-        if length(λ_overlap) > 0
-            _, channel_masks = cubefitter_mir_get_n_channels(λ_overlap, cube_fitter.z)
-        else
-            channel_masks = [falses(0) for _ in 1:length(cube_fitter.channel_masks)]
-        end
-        # Pad the channel masks to the left and right to get them the right size and to include the left/right edges
-        # in the appropriate channel masks
-        for i ∈ eachindex(channel_masks)
-            if i == 1
-                channel_masks[i] = [trues(L_left); channel_masks[i]; pad_right]
-            elseif i == length(channel_masks)
-                channel_masks[i] = [pad_left; channel_masks[i]; trues(L_right)]
+        channel_masks = Vector{BitVector}()
+        for i ∈ eachindex(cube_fitter.channel_masks)
+            bounds = extrema(cube_fitter.cube.λ[cube_fitter.channel_masks[i]])
+            dλ = mean(diff(cube_fitter.cube.λ[cube_fitter.channel_masks[i]]))
+            if i == 1 && length(cube_fitter.channel_masks) == 1
+                mask_i = trues(length(λ))
+            elseif i == 1
+                mask_i = λ .< (bounds[2]+dλ/2)
+            elseif i == length(cube_fitter.channel_masks)
+                mask_i = λ .> (bounds[1]-dλ/2)
             else
-                channel_masks[i] = [pad_left; channel_masks[i]; pad_right]
+                mask_i = (bounds[1]-dλ/2) .< λ .< (bounds[2]+dλ/2)
             end
+            push!(channel_masks, mask_i)
         end
-        channel_masks = Vector{BitVector}(channel_masks)
-
     end
+
+    _evaluate_model(λ, p_cont, p_lines, N, templates_spax, channel_masks, cube_fitter)
+end
+
+
+# Method for using apertures
+function evaluate_model(λ::Vector{<:Real}, aperture::Aperture.AbstractAperture, cube_fitter::CubeFitter, 
+    output_path::String, cube_fitter_nuc::Union{CubeFitter,Nothing}=nothing, output_path_nuc::Union{String,Nothing}=nothing)
+    apertures = repeat([aperture], length(cube_fitter.cube.λ))
+    evaluate_model(λ, apertures, cube_fitter, output_path, cube_fitter_nuc, output_path_nuc)
+end
+
+
+# Method for using apertures
+function evaluate_model(λ::Vector{<:Real}, aperture::Union{Vector{<:Aperture.AbstractAperture},String}, cube_fitter::CubeFitter, 
+    output_path::String, cube_fitter_nuc::Union{CubeFitter,Nothing}=nothing, output_path_nuc::Union{String,Nothing}=nothing)
+
+    # Loop through each wavelength pixel and perform the aperture photometry
+    shape = (1,1,size(cube_fitter.cube.I, 3))
+    if aperture isa String
+        @assert lowercase(aperture) == "all" "The only accepted string input for 'aperture' is 'all' to signify the entire cube."
+        I, σ, templates, area_sr = get_total_integrated_intensities(cube_fitter; shape=shape)
+    else
+        I, σ, templates, area_sr = get_aperture_integrated_intensities(cube_fitter, shape, aperture)
+    end
+    cube_data = (λ=cube_fitter.cube.λ, I=I, σ=σ, templates=templates, area_sr=area_sr)
+
+    results = readdlm(joinpath(output_path, "spaxel_binaries", "spaxel_1_1.csv"), ',', Float64, '\n')
+    p_out = results[:, 1]
+    p_err = results[:, 2:3]
+    
+    # Split up into continuum & line parameters
+    split1 = cube_fitter.n_params_cont
+    split2 = cube_fitter.n_params_cont + cube_fitter.n_params_lines
+    p_cont = p_out[1:split1]
+    p_lines = p_out[split1+1:split2]
+
+    # Get the normalization
+    N = Float64(nanmaximum(abs.(cube_data.I[1, 1, :])))
+
+    # The nuclear template must be able to be evaluated at any point
+    templates_spax = cube_fitter.templates[1, 1, :, :]
+    channel_masks = cube_fitter.channel_masks
+
+    if !isnothing(cube_fitter_nuc)
+        # First, get the nuclear template over the whole FOV
+        fname = joinpath(output_path, "$(cube_fitter.name)_full_model.fits")
+        hdu_model = FITS(fname)
+        agn_templates = read(hdu_model["TEMPLATES.NUCLEAR"]) .* nanmaximum(nansum(.~cube_fitter_nuc.cube.mask, dims=(1,2)))
+
+        I_nuc = evaluate_post_fit_nuclear_template_model(λ, cube_fitter_nuc, agn_templates, output_path_nuc)
+
+        # Read in the parameter maps
+        fname = joinpath(output_path, "$(cube_fitter.name)_parameter_maps.fits")
+        hdu_param = FITS(fname)
+
+        # first combine into 1d array, then renormalize
+        if aperture isa String
+            @assert lowercase(aperture) == "all" "The only accepted string input for 'aperture' is 'all' to signify the entire cube."
+            I_nuc_spax = I_nuc
+        else
+            psf_norm = copy(cube_fitter.cube.psf_model)
+            psf_norm_1d = zeros(size(psf_norm, 3))
+            for (i, ch_mask) ∈ enumerate(cube_fitter.channel_masks)
+                fit_norm = 10 .^ read(hdu_param["TEMPLATES.NUCLEAR.AMP_$i"])[1,1]      # Fit amplitudes
+                for k ∈ findall(ch_mask)
+                    s = nansum(psf_norm[:, :, k] .* fit_norm) 
+                    psf_norm[:, :, k] ./= s                                     
+                    psf_norm_1d[k] = photometry(aperture[k], psf_norm[:, :, k]).aperture_sum  # Fraction of the total PSF
+
+                    # Need to convert I_nuc from an average over the whole FOV to an average over just the aperture
+                    #  --> multiply by area of all spaxels and divide by area of aperture
+                    psf_norm_1d[k] *= nansum(.~cube_fitter.cube.mask[:,:,k])
+                    psf_norm_1d[k] /= get_area(aperture[k])
+                end                                                                  
+            end
+            psf_norm_spax = Spline1D(cube_fitter.cube.λ, psf_norm_1d; s=0., bc="nearest")
+            I_nuc_spax = I_nuc .* psf_norm_spax(λ)
+        end
+
+        templates_spax = Matrix{Float64}(undef, length(λ), 1)
+        templates_spax[:, 1] .= I_nuc_spax
+
+        # Create new channel masks carefully -- dont add new channel masks if the extrapolation extends into other 
+        # wavebands that could've been covered, just add them into the existing channel masks
+        channel_masks = Vector{BitVector}()
+        for i ∈ eachindex(cube_fitter.channel_masks)
+            bounds = extrema(cube_fitter.cube.λ[cube_fitter.channel_masks[i]])
+            dλ = mean(diff(cube_fitter.cube.λ[cube_fitter.channel_masks[i]]))
+            if i == 1 && length(cube_fitter.channel_masks) == 1
+                mask_i = trues(length(λ))
+            elseif i == 1
+                mask_i = λ .< (bounds[2]+dλ/2)
+            elseif i == length(cube_fitter.channel_masks)
+                mask_i = λ .> (bounds[1]-dλ/2)
+            else
+                mask_i = (bounds[1]-dλ/2) .< λ .< (bounds[2]+dλ/2)
+            end
+            push!(channel_masks, mask_i)
+        end
+    end
+
+    _evaluate_model(λ, p_cont, p_lines, N, templates_spax, channel_masks, cube_fitter)
+end
+
+
+
+# Internal helper function used by all iterations of evaluate_model
+function _evaluate_model(λ::Vector{<:Real}, p_cont::Vector{<:Real}, p_lines::Vector{<:Real}, N::Real, 
+    templates_spax::Matrix{<:Real}, channel_masks::Vector{BitVector}, cube_fitter::CubeFitter)
 
     # Evaluate the model at the given wavelength
     if cube_fitter.spectral_region == :MIR
@@ -1922,6 +2020,7 @@ function evaluate_model(λ::Vector{<:Real}, spaxel::CartesianIndex, cube_fitter:
             comps[comp] .*= N
         end
     end
+
     I_model, comps
 end
 
@@ -1950,45 +2049,7 @@ function evaluate_post_fit_nuclear_template_model(λ::Vector{<:Real}, cube_fitte
     # Should be empty
     templates_spax = cube_data.templates[1,1,:,:]
 
-    # Evaluate the model at the given wavelength
-    if cube_fitter.spectral_region == :MIR
-        ext_key = "extinction"
+    I_model, _ = _evaluate_model(λ, p_cont, p_lines, N, templates_spax, cube_fitter.channel_masks, cube_fitter)
 
-        I_cont, comps_cont = model_continuum(λ, p_cont, N, cube_fitter.n_dust_cont, cube_fitter.n_power_law, 
-            cube_fitter.dust_features.profiles, cube_fitter.n_abs_feat, cube_fitter.extinction_curve, cube_fitter.extinction_screen, 
-            cube_fitter.κ_abs, cube_fitter.custom_ext_template, cube_fitter.fit_sil_emission, cube_fitter.fit_temp_multexp, false, 
-            templates_spax, cube_fitter.channel_masks, false, true)
-    else
-        ext_key = "attenuation_gas"
-
-        # The velocity scale and systemic velocity offsets depend on the wavelength vector and must be recalculated
-        @assert isapprox((λ[2]/λ[1]), (λ[end]/λ[end-1]), rtol=1e-6) "Input wavelength vector must be logarithmically binned to fit optical data!"
-        vres = log(λ[2]/λ[1]) * C_KMS
-        vsyst_ssp = log(cube_fitter.ssp_λ[1]/λ[1]) * C_KMS
-        _, feii_λ, _, _ = generate_feii_templates(cube_fitter.cube.λ, cube_fitter.cube.lsf)
-        vsyst_feii = log(feii_λ[1]/λ[1]) * C_KMS
-
-        # Get the stellar templates
-        stellar_templates = nothing
-        if cube_fitter.spectral_region == :OPT
-            stellar_templates = precompute_stellar_templates(cube_fitter, p_cont, trues(length(p_cont)))
-        end
-
-        I_cont, comps_cont = model_continuum(λ, p_cont, N, vres, vsyst_ssp, vsyst_feii, cube_fitter.npad_feii,
-            cube_fitter.n_ssps, cube_fitter.ssp_λ, stellar_templates, cube_fitter.feii_templates_fft, cube_fitter.n_power_law, 
-            cube_fitter.fit_uv_bump, cube_fitter.fit_covering_frac, cube_fitter.fit_opt_na_feii, cube_fitter.fit_opt_br_feii, 
-            cube_fitter.extinction_curve, templates_spax, cube_fitter.fit_temp_multexp, false, true)
-    end
-
-    # Get the extinction curve and LSF interpolation function necessary for the line model function
-    ext_curve = comps_cont[ext_key]
-    lsf_interp = Spline1D(cube_fitter.cube.λ, cube_fitter.cube.lsf, k=1)
-    lsf_interp_func = x -> lsf_interp(x)    # Interpolate the LSF
-
-    I_lines, comps_lines = model_line_residuals(λ, p_lines, cube_fitter.n_lines, cube_fitter.n_comps, cube_fitter.lines, 
-        cube_fitter.flexible_wavesol, ext_curve, lsf_interp_func, cube_fitter.relative_flags, nothing, false, true) 
-    
-    # Combine continuum + lines and renormalize: This gives the nuclear template over the whole FOV, evaluated at a 
-    # novel wavelength vector
-    I_model = (I_cont .+ I_lines) .* N
+    I_model
 end
